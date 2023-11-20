f5d4d0f5ed36e12f25a29b03c45455288a38a5a7
Summer <blue.devtest@protonmail.com>
2023-11-20 21:02:03 +0800

    Modified User submission     Core Governance dropzone     Essential Governance in progress with dropzone.js

--- src/plugins/dropzone/src/dropzone.js
+++ src/plugins/dropzone/src/dropzone.js
@@ -410,7 +410,7 @@ class Dropzone extends Emitter {
       /**
        * The text used before any files are dropped.
        */
-      dictDefaultMessage: "Drop files here to upload",
+      dictDefaultMessage: "Drag and drop to upload",
 
       /**
        * The text that replaces the default message text it the browser is not supported.
